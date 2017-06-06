b03d689e5398df0bbb8a89683fe64fb844ffd6d4
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-06-07 06:46:26 +0700

    Hide Google Sheet URL in frontpage

--- tpl/footer.php
+++ tpl/footer.php
@@ -4,7 +4,7 @@
 	<small><i>
 		Created by <a href="https://github.com/saifulwebid/">Muhammad Saiful Islam - 141524020</a><br>
 		Free hosted in <a href="https://imagine.microsoft.com">Microsoft Imagine (formerly Dreamspark)</a><br>
-		Data fetched realtime from <a href="https://docs.google.com/spreadsheets/d/<?php echo SPREADSHEET_ID; ?>/edit?usp=sharing">https://docs.google.com/spreadsheets/d/<?php echo SPREADSHEET_ID; ?>/edit?usp=sharing</a>
+		Data fetched realtime from Google Sheets
 	</i></small>
 </p>
 
