8e231d03a87acd19050603db741040562b88695c
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-06-07 12:37:29 +0700

    Change the way to render each row in frontpage

--- index.php
+++ index.php
@@ -28,31 +28,20 @@ include('tpl/header.php');
 		</tr>
 	</thead>
 	<tbody>
-<?php
-
-$index = 1;
-foreach ($companies as $company)
-{
-	printf('
+		<?php $index = 1; foreach ($companies as $company) : ?>
 		<tr>
-			<td align="center">%d</td>
-			<td>%s</td>
-			<td>%s</td>
-			<td>%s</td>
-			<td align="center">%d</td>
-			<td>%s</td>
-			<td>%s</td>
-			<td>%s</td>
-			<td>%s</td>
-			<td><a href="perusahaan.php?id=%d">Lihat peminat</a></td>
-		</tr>',
-		$company['index'], $company['name'], $company['location'],
-		$company['project'], $company['quota'], $company['prerequisites'],
-		$company['knowledgeArea'], $company['coach'], $company['facilities'],
-		$company['index']);
-}
-
-?>
+			<td align="center"><?php echo $index++; ?></td>
+			<td><?php echo $company['name']; ?></td>
+			<td><?php echo $company['location']; ?></td>
+			<td><?php echo $company['project']; ?></td>
+			<td align="center"><?php echo $company['quota']; ?></td>
+			<td><?php echo $company['prerequisites']; ?></td>
+			<td><?php echo $company['knowledgeArea']; ?></td>
+			<td><?php echo $company['coach']; ?></td>
+			<td><?php echo $company['facilities']; ?></td>
+			<td><a href="perusahaan.php?id=<?php echo $company['']; ?>">Lihat peminat</a></td>
+		</tr>
+		<?php endforeach; ?>
 	</tbody>
 </table>
 
