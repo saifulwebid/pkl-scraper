b7f51eaffcc29d990e8f5ec9fef8cafe2e880a11
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-06-08 05:19:45 +0700

    Add repository link in footer

--- tpl/footer.php
+++ tpl/footer.php
@@ -1,11 +1,13 @@
 <hr>
 
 <p align="right">
-	<small><i>
-		Created by <a href="https://github.com/saifulwebid/">Muhammad Saiful Islam - 141524020</a><br>
-		Free hosted in <a href="https://imagine.microsoft.com">Microsoft Imagine (formerly Dreamspark)</a><br>
-		Data fetched realtime from Google Sheets
-	</i></small>
+	<small>
+		<i>
+			Created by <a href="https://github.com/saifulwebid/pkl-scraper">Muhammad Saiful Islam - 141524020</a><br>
+			Free hosted in <a href="https://imagine.microsoft.com">Microsoft Imagine (formerly Dreamspark)</a><br>
+			Data fetched realtime from Google Sheets
+		</i>
+	</small>
 </p>
 
 </body>
